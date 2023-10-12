3ca3af7956a9fb80819878b3bca4d8dacd9dc212
Dominic Kenny <dominicjkenny@gmail.com>
2023-10-12 16:09:36 -0400

    controller and server
    Co-authored-by: swarna2072 <swarna2072@users.noreply.github.com>
    Co-authored-by: conniedonahue <conniedonahue@users.noreply.github.com>
    Co-authored-by: johnlcos <johnlcos@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,85 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser"); //outdated
-const cors = require("cors");
-const { Pool } = require("pg");
-const e = require("express"); // ???
-const path = require("path");
-const fs = require("fs");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+const userController = require('./userController.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // keep open
 
 app.use(cors()); // look into
-app.use(bodyParser.json()); //oudated (app.use(express.json()))
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const pool = new Pool({
-  connectionString: process.env.URL,
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Dating App"); // You can customize this response
+app.get('/', (req, res) => {
+  res.send('Welcome to Dating App'); // You can customize this response
 });
 
 //register USER  /// move to controller
-app.post("/register", async (req, res) => {
-  const { username, password, personalInterests } = req.body;
-
-  try {
-    // Insert the user into the 'users' table
-    const newUser = await pool.query(
-      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
-      [username, password]
-    );
-
-    // Extract the newly generated user ID
-    const userId = newUser.rows[0].id;
-
-    // Insert user's personal interests into the 'personal_interests' table
-    if (Array.isArray(personalInterests)) {
-      for (const personalInterest of personalInterests) {
-        await pool.query(
-          "INSERT INTO personal_interests (user_id, interest) VALUES ($1, $2)",
-          [userId, personalInterest]
-        );
-      }
-      res.status(201).json({ message: "Registration successful" });
-    } else {
-      //dress this up pretty
-      res.status(400).json({ error: "personalInterests should be an array" });
-    }
-  } catch (error) {
-    console.error("Error during registration:", error);
-    res.status(500).json({ error: "Registration failed" });
-  }
+app.post('/signup', userController.signUpUser, (req, res) => {
+  return res.status(200).json(res.locals.newUser);
 });
 
 //LOGIN  --> test if user exists (for now, later check for password)
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    // Check if the user exists in the database
-    const user = await pool.query("SELECT * FROM users WHERE username = $1", [
-      username,
-    ]);
-
-    if (user.rows.length === 0) {
-      return res.status(401).json({ message: "Authentication failed" });
-    } else {
-      //sends back first instance of user match
-      res.json(user.rows[0]);
-    }
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+app.post('/login', userController.logInUser, (req, res) => {
+  return res.status(200).json(res.locals.userInfo);
 });
 
 ///This might be where we fix the SIKE page
-app.get("/matches", async (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../src/components/Matches.jsx"));
+app.get('/matches', userController.getMatches, (req, res) => {
+  return res.status(200).json(res.locals.matches);
 });
 
 // app.get('/interests', async (req, res) => {
@@ -88,11 +37,11 @@ app.get("/matches", async (req, res) => {
 // });
 
 //sike page:
-app.get("/connect", async (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../src/SIKE.html"));
+app.get('/connect', async (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../src/SIKE.html'));
 });
 
-app.get("/search", async (req, res) => {
+app.get('/search', async (req, res) => {
   //expect input
   //expect output
   const { preference1, preference2, preference3 } = req.query;
@@ -106,7 +55,7 @@ app.get("/search", async (req, res) => {
     for (const preference of interestArr) {
       const usersWithInterest = await pool.query(
         //keep same query string we were using before ->
-        "SELECT users.username, personal_interests.interest FROM users JOIN personal_interests ON users.id = personal_interests.user_id WHERE personal_interests.interest = $1",
+        'SELECT users.username, personal_interests.interest FROM users JOIN personal_interests ON users.id = personal_interests.user_id WHERE personal_interests.interest = $1',
         [preference]
       );
 
@@ -119,7 +68,7 @@ app.get("/search", async (req, res) => {
 
         // If the user already exists in userInterests, update their interests
         if (userInterests[username]) {
-          userInterests[username].push(" , " + interest);
+          userInterests[username].push(' , ' + interest);
         } else {
           // If the user doesn't exist, create  new entry
           userInterests[username] = [interest];
@@ -138,17 +87,32 @@ app.get("/search", async (req, res) => {
     //RESPONSE
     //no users found check
     if (output.length === 0) {
-      return res.status(404).json({ message: "No users found" });
+      return res.status(404).json({ message: 'No users found' });
     } else {
       //if users found, we need to write output to storage.txt
-      fs.writeFileSync("./server/public/storage.txt", JSON.stringify(output));
-      res.redirect("http://localhost:8080/"); // redirect to '/' route
+      fs.writeFileSync('./server/public/storage.txt', JSON.stringify(output));
+      res.redirect('http://localhost:8080/'); // redirect to '/' route
       // res.status(200).send("Hi Hadrian");
     }
   } catch (error) {
-    console.error("Error during search:", error);
-    res.status(500).json({ error: "Search failed" });
+    console.error('Error during search:', error);
+    res.status(500).json({ error: 'Search failed' });
   }
+});
+
+// Unknown route handler
+app.use('*', (req, res) => res.status(404));
+
+//Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 const server = app.listen(PORT, () => {
--- server/userController.js
+++ server/userController.js
@@ -1,42 +1,139 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+const URI = process.env.URI;
+
 const userController = {};
+
+const pool = new Pool({
+  connectionString: URI,
+});
+
+console.log('process.env: ', URI);
 
 //User middleware
 
-userController.registerUser = async (req, res, next) => {
-  const { username, password, personalInterests } = req.body;
+userController.signUpUser = async (req, res, next) => {
+  const { username, password, lang, focus, skill } = req.body;
+
+  //handle case of missing username or password in req.body
+  if (!username || !password) {
+    return next({
+      log: `userController.logInUser: ERROR: no username or pw in req.body`,
+      message: `Error: username and/or password requried`,
+      status: 412,
+    });
+  }
 
   try {
     // Insert the user into the 'users' table
-    const newUser = await pool.query(
-      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
-      [username, password]
+    const createNewUser = await pool.query(
+      'INSERT INTO users (username, password, focus, skill_level) VALUES ($1, $2, $3, $4) RETURNING username, focus, skill_level',
+      [username, password, focus, skill]
     );
 
-    // Extract the newly generated user ID
-    const userId = newUser.rows[0].id;
-
-    // Insert user's personal interests into the 'personal_interests' table
-    if (Array.isArray(personalInterests)) {
-      for (const personalInterest of personalInterests) {
-        await pool.query(
-          "INSERT INTO personal_interests (user_id, interest) VALUES ($1, $2)",
-          [userId, personalInterest]
-        );
-      }
-      return next();
-    } else {
-      res.status(400).json({ error: "personalInterests should be an array" });
-      return next({
-        log: `personalInterests should be an array: ${err}`,
-        status: 500,
-        message: { err: "Invalid personalInterests" },
-      });
-    }
-  } catch (err) {
+    // Extract the newly generated user object
+    const newUser = createNewUser.rows[0];
+    res.locals.newUser = newUser;
+    return next();
+  } catch (error) {
     return next({
-      log: `Error during registration: ${err}`,
+      log: `userController.signUpUser: ERROR: ${error}`,
+      message: `${error}: unable to create user`,
       status: 500,
-      message: { err: "Registration failed" },
     });
   }
 };
+
+userController.logInUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  //handle case of missing username or password in req.body
+  if (!username || !password) {
+    return next({
+      log: `userController.logInUser: ERROR: no username or pw in req.body`,
+      message: `Error: invalid input`,
+      status: 412,
+    });
+  }
+
+  // get user matching username and password
+  try {
+    const checkUser = await pool.query(
+      `SELECT username, focus, skill_level FROM users WHERE username = $1 AND password = $2`,
+      [username, password]
+    );
+
+    // check return value to see if the login was correct
+    if (checkUser.length > 1) {
+      return next({
+        log: `userController.logInUser: ERROR: more than one user with that username or password`,
+        message: `Could not log in`,
+        status: 500,
+      });
+    } else if (checkUser.length === 0) {
+      return next({
+        log: `userController.logInUser: ERROR: username or password incorrect`,
+        message: `Username and/or password incorrect.`,
+        status: 412,
+      });
+    } else {
+      // save user data into res.locals
+      res.locals.userInfo = checkUser.rows[0];
+      return next();
+    }
+  } catch (error) {
+    return next({
+      log: `userController.logInUser: ERROR: ${error}`,
+      message: `${error}: unable to log user in`,
+      status: 500,
+    });
+  }
+};
+
+userController.getMatches = async (req, res, next) => {
+  const { username } = req.params;
+
+  // handle case of no username in params
+  if (!username) {
+    return next({
+      log: `userController.getMatches: ERROR: no username in req.params`,
+      message: `Error: invalid input`,
+      status: 412,
+    });
+  }
+
+  // get user data from database
+  try {
+    const getMatches = await pool.query(
+      `
+    WITH mainUser AS (
+      SELECT username, skill_level, focus FROM  users
+      WHERE  username = $1
+    ) 
+    SELECT username, skill_level, focus from users 
+    WHERE users.focus = mainUser.focus OR _users.skill_level = mainUser.skill_level AND users.username <> mainUser.username;`,
+      [username]
+    );
+    // save match data in res.locals
+    res.locals.matches = getMatches.row;
+    return next();
+  } catch (error) {
+    return next({
+      log: `userController.getMatches: ERROR: ${error}`,
+      message: `${error}: unable to access matches`,
+      status: 500,
+    });
+  }
+};
+
+module.exports = userController;
+//OLD CODE FOR SECOND TABLE INSERT
+// // Insert user's personal interests into the 'personal_interests' table
+// if (Array.isArray(personalInterests)) {
+//   for (const personalInterest of personalInterests) {
+//     await pool.query(
+//       "INSERT INTO personal_interests (user_id, interest) VALUES ($1, $2)",
+//       [userId, personalInterest]
+//     );
+//   }
